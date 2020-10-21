908444e7db5ca2751543e3e51591a7af5038f217
Wilson Ramirez Zuluaga <wilson.rz@gmail.com>
2020-10-21 09:35:14 -0500

    prueba4

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def init():
